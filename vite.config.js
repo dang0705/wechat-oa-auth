5b9c53893d7357fd8174f8c90f03f66583484ad3
应用组-于杰 <yujie@yiban.cn>
2022-08-22 12:05:05 +0800

    chore(vite.config.js): lib.formats only accept 'umd'

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, './index.js'),
-      name: 'wechat-oa-auth'
+      name: 'wechat-oa-auth',
+      formats: ['umd']
     }
   }
 });
